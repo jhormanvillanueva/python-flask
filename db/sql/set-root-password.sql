bded9eae2fde72a047959322248646611c3d13aa
Jhorman Villanueva <jhorvi24@hotmail.com>
2024-09-17 09:35:15 -0500

    ok for presentation

--- db/sql/set-root-password.sql
+++
@@ -1,6 +0,0 @@
---
--- Creates a root user that can connect from any host and sets the password for all root users in Mariadb
---
-USE mysql;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'Test!2024' WITH GRANT OPTION;
-flush privileges;
